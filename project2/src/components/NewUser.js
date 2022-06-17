f173437d429e42f21a257abee5490d069d0da5b6
Victor Sandru <vsandru4@gmail.com>
2022-06-17 17:06:45 -0400

    lecture 90

---
+++ project2/src/components/NewUser.js
@@ -0,0 +1,14 @@
+const NewUser = () => {
+    return (
+        <div>
+            <form>
+                <label>Username:</label>
+                <input type="text"></input>
+                <label>Age:</label>
+                <input type="text"></input>
+            </form>
+        </div>
+    )
+}
+
+export default NewUser;
---
+++ project2/src/components/User.js
@@ -0,0 +1,11 @@
+import styles from './User.modules.css';
+import NewUser from './NewUser';
+
+const User = () => {
+    <div>
+        <NewUser />
+    </div>
+
+}
+
+export default User;
